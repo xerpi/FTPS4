cd2e5826117458956f73cfe0e857d5db1de1d8ef
Mistawes <wes.moore@gmail.com>
2018-03-19 14:06:23 +0000

    Updated for 4.55

--- source/main.c
+++ source/main.c
@@ -1,9 +1,25 @@
 /*
- * Copyright (c) 2015 Sergi Granell (xerpi)
- */
+* Copyright (c) 2015 Sergi Granell (xerpi)
+*/
+/*****************************************************************
+*
+* ===== Root FTP + UART + Debug settings for 4.55 - Mistawes =====
+*
+*	Thanks to:
+*	-qwertyuiop for his kernel exploit / Specter for his Code Execution method
+*	-IDC for his patches
+*	-Grass Skeu for his original Dump File on 1.76 that most
+*	of this code came from, thanks Skeu!
+*	-Mistawes for converting this to FTP
+*
+******************************************************************/
 
 #include "ps4.h"
 #include "ftps4.h"
+
+// Defines
+
+#define IP(a, b, c, d) (((a) << 0) + ((b) << 8) + ((c) << 16) + ((d) << 24))
 
 #undef  SHOW_DEBUG
 #define PS4_IP   "192.168.0.22\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
@@ -11,14 +27,24 @@
 #define LOG_IP   "192.168.0.28\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
 #define LOG_PORT 9023
 
-// DebugSettings
-#define IP(a, b, c, d) (((a) << 0) + ((b) << 8) + ((c) << 16) + ((d) << 24))
-
-
 #define	CTL_KERN	1	/* "high kernel": proc, limits */
 #define	KERN_PROC	14	/* struct: process entries */
 #define	KERN_PROC_VMMAP	32	/* VM map entries for process */
 #define	KERN_PROC_PID	1	/* by process id */
+
+#define TRUE 1
+#define FALSE 0
+
+#define X86_CR0_WP (1 << 16)
+
+#define printfsocket(format, ...)\
+	do {\
+		char buffer[512];\
+		int size = sprintf(buffer, format, ##__VA_ARGS__);\
+		sceNetSend(sock, buffer, size, 0);\
+	} while(0)
+
+int sock;
 
 struct auditinfo_addr {
     /*
@@ -30,14 +56,6 @@ struct auditinfo_addr {
     */
     char useless[184];
 };
-
-#define printfsocket(format, ...)\
-	do {\
-		char buffer[512];\
-		int size = sprintf(buffer, format, ##__VA_ARGS__);\
-		sceNetSend(sock, buffer, size, 0);\
-	} while(0)
-
 
 unsigned int long long __readmsr(unsigned long __register) {
 	// Loads the contents of a 64-bit model specific register (MSR) specified in
@@ -52,8 +70,6 @@ unsigned int long long __readmsr(unsigned long __register) {
 	return (((unsigned int long long)__edx) << 32) | (unsigned int long long)__eax;
 }
 
-
-#define X86_CR0_WP (1 << 16)
 
 static inline __attribute__((always_inline)) uint64_t readCr0(void) {
 	uint64_t cr0;
@@ -117,9 +133,11 @@ struct kpayload_args{
 	uint64_t user_arg;
 };
 
-// DebugSettings Definitions end here
+struct kdump_args{
+    	uint64_t argArrayPtr;
+};
 
-int kpayload(struct thread *td, struct kpayload_args* args){
+int kpayload(struct thread *td){
 
 	struct ucred* cred;
 	struct filedesc* fd;
@@ -127,15 +145,14 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 	fd = td->td_proc->p_fd;
 	cred = td->td_proc->p_ucred;
 
-	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-0x30EB30];
+	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-0x3095D0];
 	uint8_t* kernel_ptr = (uint8_t*)kernel_base;
-	void** got_prison0 =   (void**)&kernel_ptr[0xF26010];
-	void** got_rootvnode = (void**)&kernel_ptr[0x206D250];
+	void** got_prison0 =   (void**)&kernel_ptr[0x10399B0];
+	void** got_rootvnode = (void**)&kernel_ptr[0x21AFA30];
 
 	// resolve kernel functions
 
-	int (*copyout)(const void *kaddr, void *uaddr, size_t len) = (void *)(kernel_base + 0x286d70);
-	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x347580);
+	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x17F30);
 
 	cred->cr_uid = 0;
 	cred->cr_ruid = 0;
@@ -144,59 +161,61 @@ int kpayload(struct thread *td, struct kpayload_args* args){
 
 	cred->cr_prison = *got_prison0;
 	fd->fd_rdir = fd->fd_jdir = *got_rootvnode;
-	
-	// uart enabler
-	*(char *)(kernel_base + 0x186b0a0) = 0; // set the console disable console output bool
 
-	// specters debug settings patchs
-	*(char *)(kernel_base + 0x186b0a0) = 0; 
-	*(char *)(kernel_base + 0x2001516) |= 0x14;
-	*(char *)(kernel_base + 0x2001539) |= 1;
-	*(char *)(kernel_base + 0x2001539) |= 2;
-	*(char *)(kernel_base + 0x200153A) |= 1;
-	*(char *)(kernel_base + 0x2001558) |= 1;	
+	// escalate ucred privs, needed for access to the filesystem ie* mounting & decrypting files
+	void *td_ucred = *(void **)(((char *)td) + 304); // p_ucred == td_ucred
+	
+	// sceSblACMgrIsSystemUcred
+	uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
+	*sonyCred = 0xffffffffffffffff;
+	
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *sceProcType = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceProcType = 0x3801000000000013; // Max access
+	
+	// sceSblACMgrHasSceProcessCapability
+	uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
+	*sceProcCap = 0xffffffffffffffff; // Sce Process
+
 
 	// Disable write protection
 
 	uint64_t cr0 = readCr0();
 	writeCr0(cr0 & ~X86_CR0_WP);
 
-	// debug menu full patches thanks to sealab
+	// enable uart :)
+	*(char *)(kernel_base + 0x1997BC8) = 0; 
 
-	*(uint32_t *)(kernel_base + 0x4CECB7) = 0;
-	*(uint32_t *)(kernel_base + 0x4CFB9B) = 0;
+	//Full debug settings offsets for 4.55 
+	*(char *)(kernel_base + 0x1B6D086) |= 0x14;
+	*(char *)(kernel_base + 0x1B6D0A9) |= 0x3;
+	*(char *)(kernel_base + 0x1B6D0AA) |= 0x1;
+	*(char *)(kernel_base + 0x1B6D0C8) |= 0x1;
 
-	// Target ID Patches :)
+	// debug menu full patches
+	*(uint32_t *)(kernel_base + 0x4D70F7) = 0;
+	*(uint32_t *)(kernel_base + 0x4D7F81) = 0;
 
-	*(uint16_t *)(kernel_base + 0x1FE59E4) = 0x8101;
-	*(uint16_t *)(kernel_base + 0X1FE5A2C) = 0x8101;
-	*(uint16_t *)(kernel_base + 0x200151C) = 0x8101;
+	// restore write protection
 
-	// Say hello and put the kernel base in userland so we can use later
+	writeCr0(cr0);
+
+	// Say hello and put the kernel base just for reference
 
 	printfkernel("\n\n\nHELLO FROM YOUR KERN DUDE =)\n\n\n");
-
 	printfkernel("kernel base is:0x%016llx\n", kernel_base);
 
-	uint64_t uaddr;
-	memcpy(&uaddr,&args[2],8);
-
-	printfkernel("uaddr is:0x%016llx\n", uaddr);
 
 	return 0;
 }
 
-// DebugSettings END
-// DebugSettings END
-// DebugSettings END
-// DebugSettings END
-
-int sock;
+// FTP stuff
+int log_sock;
 int run;
 
 static void info_log(const char *s)
 {
-	sceNetSend(sock, s, strlen(s), 0);
+	sceNetSend(log_sock, s, strlen(s), 0);
 }
 #define INFO(...) \
 	do { \
@@ -208,7 +227,7 @@ static void info_log(const char *s)
 #ifdef SHOW_DEBUG
 static void debug_log(const char *s)
 {
-	sceNetSend(sock, s, strlen(s), 0);
+	sceNetSend(log_sock, s, strlen(s), 0);
 }
 #define DEBUG(...) \
 	do { \
@@ -300,31 +319,29 @@ void custom_UMT(ftps4_client_info_t *client)
 
 	ftps4_ext_client_send_ctrl_msg(client, "200 Unmount success." FTPS4_EOL);
 }
+// End of FTP stuff
 
 
 int _main(struct thread *td){
 
 	run = 1;
+
 	// Init and resolve libraries
 	initKernel();
 	initLibc();
 	initNetwork();
 	initPthread();
 
-	// create our server
+	// Init netdebug
 	struct sockaddr_in server;
-
 	server.sin_len = sizeof(server);
 	server.sin_family = AF_INET;
 	sceNetInetPton(AF_INET, LOG_IP, &server.sin_addr);
 	server.sin_port = sceNetHtons(LOG_PORT);
 	memset(server.sin_zero, 0, sizeof(server.sin_zero));
-	sock = sceNetSocket("netdebug", AF_INET, SOCK_STREAM, 0);
-	sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server));
 
-	printfsocket("connected\n");
-
-	// kdump payload loop woz 'ere
+	log_sock = sceNetSocket("netdebug", AF_INET, SOCK_STREAM, 0);
+	sceNetConnect(log_sock, (struct sockaddr *)&server, sizeof(server));
 
 	ftps4_set_info_log_cb(info_log);
 #ifdef SHOW_DEBUG
@@ -339,7 +356,10 @@ int _main(struct thread *td){
 
 	INFO("PS4 listening on IP %s Port %i\n", PS4_IP, PS4_PORT);
 
-	int sRet = syscall(11,kpayload,td);
+    printfsocket("Connected.\n");
+
+	// jailbreak / debug settings etc
+	syscall(11,kpayload,td);
 
 	while (run) {
 		sceKernelUsleep(5 * 1000);
@@ -349,6 +369,6 @@ int _main(struct thread *td){
 
 	INFO("Bye!");
 
-	sceNetSocketClose(sock);
+	sceNetSocketClose(log_sock);
 	return 0;
 }
